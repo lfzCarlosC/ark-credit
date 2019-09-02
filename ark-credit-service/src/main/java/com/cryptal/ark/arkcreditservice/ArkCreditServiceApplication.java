54b2967e998e7edb4862648b2c6c317eafbbd084
Fangzhou Li <fangzhou@brandwatch.com>
2019-09-02 23:48:32 +0800

    add ark-credit-service module

--- ark-credit-service/src/main/java/com/cryptal/ark/arkcreditservice/ArkCreditServiceApplication.java
+++ ark-credit-service/src/main/java/com/cryptal/ark/arkcreditservice/ArkCreditServiceApplication.java
@@ -1,13 +1,13 @@
-package com.cryptal.ark.arkcredit;
+package com.cryptal.ark.arkcreditservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ArkCreditApplication {
+public class ArkCreditServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ArkCreditApplication.class, args);
+		SpringApplication.run(ArkCreditServiceApplication.class, args);
 	}
 
 }
--- ark-credit-service/src/test/java/com/cryptal/ark/arkcreditservice/ArkCreditServiceApplicationTests.java
+++ ark-credit-service/src/test/java/com/cryptal/ark/arkcreditservice/ArkCreditServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.cryptal.ark.arkcredit;
+package com.cryptal.ark.arkcreditservice;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ArkCreditApplicationTests {
+public class ArkCreditServiceApplicationTests {
 
 	@Test
 	public void contextLoads() {
