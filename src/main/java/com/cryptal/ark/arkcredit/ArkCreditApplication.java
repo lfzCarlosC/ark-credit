9f7b8aae82d1c9cdf9dedf02532f20d2c6a58273
Fangzhou Li <fangzhou@brandwatch.com>
2019-09-02 23:01:04 +0800

    add spring

---
+++ src/main/java/com/cryptal/ark/arkcredit/ArkCreditApplication.java
@@ -0,0 +1,13 @@
+package com.cryptal.ark.arkcredit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArkCreditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArkCreditApplication.class, args);
+	}
+
+}
