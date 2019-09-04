0fba7fd37f8a7d48577427fd94bcf23f34ecdf72
yangyu-ds <yangyu-ds@gome.com.cn>
2019-09-04 16:31:49 +0800

    调整pom层级结构

---
+++ ark-credit-dal/src/main/java/com/cryptal/ark/credit/dal/repository/ArkCreditApplication.java
@@ -0,0 +1,14 @@
+package com.cryptal.ark.credit.dal.repository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArkCreditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArkCreditApplication.class, args);
+	}
+
+}
+
